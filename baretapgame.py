34de9bd47cf556b040dace50f27e0f77d0ce38b3
Baretap <baretapgame@gmail.com>
2024-10-02 19:12:24 +0300

    Add files via upload

--- baretapgame.py
+++ baretapgame.py
@@ -1,8 +1,12 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Hei! Olen bottisi.')
+    keyboard = [
+        [InlineKeyboardButton("Play Game", url="https://baretap.github.io/Baretaptest1/")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Click the button below to play the game!', reply_markup=reply_markup)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('7916996723:AAG8dfY-Mv4sTN607_Ic6Wn2jR_3iGnU3r8').build()
